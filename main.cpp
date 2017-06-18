8d608a2e4fedd044e1afa3f139cebd2bcd22164f
Olya Gavrilova <lelik98_98@mail.ru>
2017-06-18 09:20:37 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include "module1.h"
+
+void main()
+
+{
+
+	string mas;//������, ���������� ��������
+
+	int sdvig, N;
+
+
+
+	cout << "Enter text for codding: ";
+
+	std::getline(std::cin, mas);//������ ��� ������ � ���������
+
+	cout << "Enter key (sdvig): ";
+
+	cin >> sdvig;
+
+	KOD box(sdvig);
+
+
+
+	int c = 1;
+
+
+
+	while (c != 0)
+
+	{
+
+		cout << "0 - exit "<<endl;
+
+		cout << "1 - KOD "<<endl;
+
+		cout << "2 - UNKOD "<<endl;
+
+		cin>>c;
+
+		if (c == 1) 
+
+		{
+
+			mas = box.shifr(mas);//�������
+
+			cout << "KOD: " << mas<<endl;
+
+		}
+
+		if (c == 2) 
+
+		{
+
+			mas = box.rashifr(mas);//����������
+
+			cout << "UNKOD: " << mas<<endl;
+
+		}
+
+	}
+
+	system("pause");
+
+}
